4471a3726ae9c9a952f4143deeb817a51b4c95d8
Fenteron <fenteron@gmail.com>
2021-08-25 08:59:47 +0000

    test

--- pastebin_api.js
+++ pastebin_api.js
@@ -19,28 +19,6 @@ async function postRequest(request, server) {
 
 let api_dev_key = 'NLlhwOjBdRWLLG_4e42chRcUXtNruVL5';
 
-class Pastebin {
-    constructor(api_dev_key, api_user_name=null, api_user_password=null) {
-	
-		this.pastebin_api_endpoint = 'https://pastebin.com/api/api_post.php';
-		this.pastebin_login_endpoint = 'https://pastebin.com/api/api_login.php';
-		this.api_user_name = api_user_name;
-		this.api_user_password = api_user_password;
-		this.api_dev_key = api_dev_key;
-		this.api_user_key = '';
-    }
-
-	request = `api_dev_key=${api_dev_key}&api_user_name=${api_user_name}&api_user_password=${api_user_password}`
-	api_user_key = postRequest(this.request, this.pastebin_login_endpoint)
-
-    createPaste(paste=null) {
-		postRequest(`api_option=paste&api_dev_key=${api_dev_key}&api_user_key=${this.api_user_key}&api_paste_code=${paste}`,
-	 	this.pastebin_api_endpoint).then((data) => {return data});
-    }
-	
-}
-
-//tests
-let paste = `Meow meow? MEOW MEOWWWW MEOOOOWWWWWWWWW`
-let pastebin = new Pastebin(api_dev_key=api_dev_key, api_user_name='CoffeeDoll', api_user_password='yhn125125arm');
-response = pastebin.createPaste(paste=paste)
+postRequest("test","https://httpbin.org/post")
+.then((response) => {return response.json();})
+.then((data) => {console.log(data);});
