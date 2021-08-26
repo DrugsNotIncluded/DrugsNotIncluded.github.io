d2a775fe4eb5df6745a7cae43ffc0967dd7be858
Fenteron <fenteron@gmail.com>
2021-08-26 08:22:27 +0000

    testing Pastebin class

--- pastebin_api.js
+++ pastebin_api.js
@@ -2,14 +2,15 @@
 
 import {postRequest,keyValueURLEncode} from './libs/methods.js';
 
+//details
 let details = {
 	'api_dev_key':'NLlhwOjBdRWLLG_4e42chRcUXtNruVL5',
 	'api_user_name':'CoffeeDoll',
 	'api_user_password':'yhn125125arm'
 };
 
-
-let params = {method:'POST',
+class Pastebin {
+    params = {method:'POST',
 	      mode:'cors',
 	      cache:'no-cache',
 	      credentials:'same-origin',
@@ -19,9 +20,28 @@ let params = {method:'POST',
 	      },
 	      redirect:'follow',
 	      referrerPolicy:'no-referrer'};
+    
+    constructor(api_dev_key='', api_user_name='', api_user_password='',cors_proxy='') {
+	this.api_dev_key = api_dev_key;
+	this.api_user_name = api_user_name;
+	this.api_user_password = api_user_password;
+	this.cors_proxy = cors_proxy;
+    }
 
-postRequest('https://pastebin.com/api/api_login.php',
-	    keyValueURLEncode(details),
-	    'https://fierce-river-56744.herokuapp.com',
-	    params)
-    .then((data) => {console.log(data);});
+    get api_user_key() {return (async () => {
+	details = {'api_dev_key':this.api_dev_key,
+		   'api_user_name':this.api_user_name,
+		   'api_user_password':this.api_user_password};
+	return await postRequest('https://pastebin.com/api/api_login.php',
+				 keyValueURLEncode(details),
+				 this.cors_proxy,
+				 this.params)
+	    .then((data) => {return data;});
+    });}
+}
+
+let paste = new Pastebin('NLlhwOjBdRWLLG_4e42chRcUXtNruVL5',
+			 'Coffeedoll',
+			 'yhn125125arm');
+
+console.log(paste.api_user_key);
