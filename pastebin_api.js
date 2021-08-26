fb404184afeb9ab69a1b830273988feab35c5fa6
Fenteron <fenteron@gmail.com>
2021-08-26 04:34:11 +0000

    bundled js test run because modules do not work for some reason

--- pastebin_api.js
+++ pastebin_api.js
@@ -1,4 +1,40 @@
-import {postRequest,keyValueURLEncode} from './libs/methods.mjs';
+//import {postRequest,keyValueURLEncode} from './libs/methods.mjs';
+
+'use strict';
+
+async function postRequest(url='',
+			   data = '',
+			   cors_proxy='',
+			   params = {method:'',
+				     mode:'',
+				     cache:'',
+				     credentials:'',
+				     headers:{},
+				     redirect:'',
+				     referrerPolicy:''}) {
+    params.body = data;
+    //cors-proxy
+    if (cors_proxy == true && cors_proxy[-1] == '/') {url = cors_proxy + "&" + url;}
+    if (cors_proxy == true && cors_proxy[-1] != '/') {url = cors_proxy + "/&" + url;}
+
+    //fetch
+    const response = await fetch(url, params);
+    return await response.text();
+    
+}
+
+function keyValueURLEncode(details) {
+    let body = [];
+    for (let property in details) {
+	let encodedKey = encodeURIComponent(property);
+	let encodedValue = encodeURIComponent(details[property]);
+	body.push(encodedKey+"="+encodedValue);
+    }
+    body = body.join("&");
+    return body;
+}
+
+//test
 
 let details = {
 	'api_dev_key':'NLlhwOjBdRWLLG_4e42chRcUXtNruVL5',
