5ea00816339eb1e4fca9b9e6dfc9f085bbb8d69c
Fenteron <fenteron@gmail.com>
2021-08-26 03:36:14 +0000

    splitted api into libraries

---
+++ libs/methods.js
@@ -0,0 +1,35 @@
+'use strict';
+
+async function postRequest(url='',
+			   data,
+			   cors_proxy='',
+			   params = {method:'',
+				     mode:'',
+				     cache:'',
+				     credentials:'',
+				     headers:{},
+				     redirect:'',
+				     referrerPolicy:''}) {
+    params.body = data;
+    //cors-proxy
+    if (cors_proxy == true && cors_proxy[-1] == '/') {url = cors_proxy + "&" + url;}
+    if (cors_proxy == true && cors_proxy[-1] != '/') {url = cors_proxy + "/&" + url;}
+
+    //fetch
+    const response = await fetch(url, params);
+    return await response.text();
+    
+}
+
+function keyValueURLEncode(details) {
+    let body = [];
+    for (let property in details) {
+	let encodedKey = encodeURIComponent(property);
+	let encodedValue = encodeURIComponent(details[property]);
+	body.push(encodedKey+"="+encodedValue);
+    }
+    body = body.join("&");
+    return body;
+}
+
+export {postRequest, keyValueURLEncode}
--- pastebin_api.js
+++ pastebin_api.js
@@ -1,54 +1,27 @@
-
-async function postRequest(url='', data='') {
-	const response = await fetch(url, {
-		method: 'POST',
-		mode: 'cors',
-		cache: 'no-cache',
-		credentials: 'same-origin',
-		headers: {
-			'Content-Type':'application/x-www-form-urlencoded',
-			'Access-Control-Allow-Origin':'*'
-		},
-		redirect: 'follow',
-		referrerPolicy: 'no-referrer',
-		body: data
-	});
-	const reader = response.body.getReader();
-	return new ReadableStream({
-		start(controller) {
-			return pump();
-			function pump() {
-				return reader.read().then(({done, value}) => {
-					if (done) {
-						controller.close();
-						return;
-					}
-					controller.enqueue(value);
-					return pump();
-				})
-			}
-		}
-	})
-	.then(stream => new Response(stream))
-	.then(response => response.blob())
-	.then(blob => URL.createObjectURL(blob))
-	.then(url => console.log(url))
-	.catch(err => console.error(err));
-}
+import {postRequest,keyValueURLEncode} from './libs/methods.js';
 
 let details = {
 	'api_dev_key':'NLlhwOjBdRWLLG_4e42chRcUXtNruVL5',
 	'api_user_name':'CoffeeDoll',
 	'api_user_password':'yhn125125arm'
 };
-let body = [];
 
-for (let property in details) {
-	let encodedKey = encodeURIComponent(property);
-	let encodedValue = encodeURIComponent(details[property]);
-	body.push(encodedKey+"="+encodedValue);
-}
-body = body.join("&")
+let detailsURLEncoded = keyValueURLEncode(details);
+let cors_proxy = 'https://fierce-river-56744.herokuapp.com/';
+let url = 'https://pastebin.com/api/api_login.php';
+let params = {method:'POST',
+	      mode:'cors',
+	      cache:'no-cache',
+	      credentials:'same-origin',
+	      headers: {
+		  'Content-Type':'application/x-www-form-urlencoded',
+		  'Access-Control-Allow-Origin':'*'
+	      },
+	      redirect:'follow',
+	      referrerPolicy:'no-referrer'};
 
-postRequest("https://fierce-river-56744.herokuapp.com/?https://pastebin.com/api/api_login.php",body)
-.then((data) => {console.log(data);});
+postRequest(url = url,
+	    data = detailsURLEncoded,
+	    cors_proxy = cors_proxy,
+	    params = params)
+    .then((data) => {console.log(data);});
