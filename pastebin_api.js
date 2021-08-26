57686ea998102638c5262153001d5180ca5c7c24
Fenteron <fenteron@gmail.com>
2021-08-26 04:29:57 +0000

    added pastebin_api back

---
+++ pastebin_api.js
@@ -0,0 +1,27 @@
+import {postRequest,keyValueURLEncode} from './libs/methods.mjs';
+
+let details = {
+	'api_dev_key':'NLlhwOjBdRWLLG_4e42chRcUXtNruVL5',
+	'api_user_name':'CoffeeDoll',
+	'api_user_password':'yhn125125arm'
+};
+
+let data = keyValueURLEncode(details);
+let cors_proxy = 'https://fierce-river-56744.herokuapp.com/';
+let url = 'https://pastebin.com/api/api_login.php';
+let params = {method:'POST',
+	      mode:'cors',
+	      cache:'no-cache',
+	      credentials:'same-origin',
+	      headers: {
+		  'Content-Type':'application/x-www-form-urlencoded',
+		  'Access-Control-Allow-Origin':'*'
+	      },
+	      redirect:'follow',
+	      referrerPolicy:'no-referrer'};
+
+postRequest(url = url,
+	    data = data,
+	    cors_proxy = cors_proxy,
+	    params = params)
+    .then((data) => {console.log(data);});
